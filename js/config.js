c371f19bee19981aa20400d43594dea214932297
saantie <saantie@gmail.com>
2026-05-13 00:02:02 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDoW_TmdkFbby9gHJTVye7pumuKsj3StxY",
+  authDomain: "finance-diary-d9d5d.firebaseapp.com",
+  projectId: "finance-diary-d9d5d",
+  storageBucket: "finance-diary-d9d5d.firebasestorage.app",
+  messagingSenderId: "1025954168396",
+  appId: "1:1025954168396:web:1092fadb48530db6337ee9"
+};
